267dda0cd7c783d966b8684dbef4f492cf125e9e
Amin Shali <amshali@gmail.com>
2012-03-23 14:18:09 -0500

    Added splitter

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,17 +1,20 @@
 package core;
 
-import java.io.FileWriter;
-import java.io.Writer;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
 
-
 public class Main {
   public static void main(String[] args) {
-    if (args.length != 3) {
-      System.out.println("Usage: input-prolog-filename velocity-file output-filename");
+    if (args.length != 2) {
+      System.out.println("Usage: input-prolog-filename velocity-file");
       System.exit(0);
     }
     try {
@@ -19,19 +22,45 @@ public class Main {
       VelocityContext context = new VelocityContext();
       Model m = new Model(args[0]);
 
-      for (String tableName: m.tables.keySet()) {
+      for (String tableName : m.tables.keySet()) {
         context.put(tableName, m.tables.get(tableName));
       }
 
       Template template = null;
 
       template = Velocity.getTemplate(args[1]);
-      Writer writer = new FileWriter(args[2]);
-      context.put("filename", args[2]);
+      StringWriter writer = new StringWriter();
 
       template.merge(context, writer);
-      writer.close();
+      if (context.get("MARKER") == null) {
+        System.out.println(writer.toString());
+      } else {
+        splitter(writer.toString(), context.get("MARKER").toString());
+      }
     } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void splitter(String content, String marker) {
+    BufferedReader br = new BufferedReader(new StringReader(content));
+    String line = null;
+    try {
+      String filename = null;
+      PrintStream out = null;
+      while ((line = br.readLine()) != null) {
+        if (line.startsWith(marker)) {
+          if (out != null) {
+            out.close();
+          }
+          filename = line.substring(marker.length());
+          out = new PrintStream(new File(filename));
+        } else {
+          if (out != null)
+            out.println(line);
+        }
+      }
+    } catch (IOException e) {
       e.printStackTrace();
     }
   }
