b285e76e024ee5a3b1e779ef148606c42480d148
QualityCan <QualityCan@gmail.com>
2012-04-14 01:25:11 -0500

    Added primary key example.

---
+++ models/introducePrimaryKey.pl
@@ -0,0 +1,16 @@
+% Classes
+% schema: clazz, id, name
+class(c1, 'A').
+class(c2, 'B').
+
+% Attributes
+% schema: attribute, id, clazz, name, type
+attribute(al1, c1, attr1, d1).
+attribute(al2, c2, attr1, d1).
+attribute(al3, c2, attr2, d3).
+
+% Data Types
+% schema: dataType, id, name
+dataType(d1, 'Boolean').
+dataType(d2, 'Integer').
+dataType(d3, 'String').
