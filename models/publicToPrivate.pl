ccbbe588db55f38020d1b77f5ce5e70d087eabb0
Amin Shali <amshali@gmail.com>
2012-04-23 12:14:36 -0500

    Added public to private

---
+++ models/publicToPrivate.pl
@@ -0,0 +1,8 @@
+
+% schema:attribute, id, name, visibility, type, scope, changeability
+attribute(att1, name, public, String, instance, changeable).
+attribute(att2, initialMarking, public, boolean, instance, changeable).
+attribute(att3, size, public, int, instance, changeable).
+attribute(att4, ratio, public, boolean, class, frozen).
+
+
