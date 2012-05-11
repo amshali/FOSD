8d72b7e84202d84db9db1d939003f800c21ba80c
Joyce Ho <joyceho@utexas.edu>
2012-05-10 21:20:04 -0500

    Script to generate output for all the examples

---
+++ src/generateExamples.py
@@ -0,0 +1,24 @@
+import os
+import fnmatch
+import subprocess
+
+# compile the main java file
+example_dir = '../examples/'
+# get the subdirectorys in the examples folder
+for root, dirs, files in os.walk(example_dir):
+    # iterate through all the examples
+    for ex_dir in dirs:
+        input_file = ''
+        template_file = ''
+        # get the input and template file
+        for file in os.listdir(example_dir + ex_dir):
+            if fnmatch.fnmatch(file, '*.pl'):
+                input_file = example_dir + ex_dir + file
+                print "Input File:", input_file
+            if fnmatch.fnmatch(file, '*.vm'):
+                template_file = example_dir + ex_dir + file
+                print "Template File:", template_file
+
+        # now we're going to run the command line tool
+        command = 'java -jar ../release/vm2t.jar ' + input_file + " " + template_file
+        os.system(command)
